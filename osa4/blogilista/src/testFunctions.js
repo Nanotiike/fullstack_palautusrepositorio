db6cb7aa49a103132b728da6dcd19fb585f2e8a8
Harri <harri.gavert@helsinki.fi>
2024-11-21 16:17:54 +0200

    exercises 4.3-4.7

---
+++ osa4/blogilista/src/testFunctions.js
@@ -0,0 +1,30 @@
+const { mostBlogs, mostLikes } = require('./utils/list_helper');
+
+const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a42akwjsakbdawd',
+      title: 'Canonical string reductio',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 12,
+      __v: 0
+    }
+  ];
+  
+  console.log(mostLikes(listWithMultipleBlogs));
